bfc960f98a47eea983d126e1c9e0bc22bf8d8d2e
friedpies <kenmarut@gmail.com>
2019-06-20 18:44:59 -0500

    styled with font

--- src/components/Launch.js
+++ src/components/Launch.js
@@ -1,16 +1,17 @@
 import React from "react";
+import { Container } from "react-bootstrap";
 import LocationFilter from "./LocationFilter";
 import "../styles/Launch.scss";
 
 const Launch = ({ onCheck, filters }) => (
   <div className="jumbotron">
-    <div className="container">
+    <Container>
       <h1 className="display-1 mb-5" id="main-header">
         pawtio
       </h1>
       <h3 className="mb-5">search for a dog-friendly place in Austin, TX</h3>
       <LocationFilter filters={filters} onCheck={onCheck} />
-    </div>
+    </Container>
   </div>
 );
 
