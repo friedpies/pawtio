539eb20ceab01484c4b07ff269333935541dbb7a
friedpies <kenmarut@gmail.com>
2019-06-20 19:19:24 -0500

    try to deploy again

--- server/index.js
+++ server/index.js
@@ -3,25 +3,25 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const port = process.env.PORT || 3002;
-const { db } = require("../database");
-const { Place } = require("../database/models");
+// const { db } = require("../database");
+// const { Place } = require("../database/models");
 
-const cors = require("cors");
+// const cors = require("cors");
 
-app.use(cors());
+// app.use(cors());
 app.use(express.static(path.join(__dirname, "../build")));
 
-app.get("/api/places", (req, res) => {
-  const filterParams = req.query;
-  const filterObj = {};
-  for (let filter in filterParams) {
-    filterObj[`filters.${filter}`] = filterParams[filter];
-  }
+// app.get("/api/places", (req, res) => {
+//   const filterParams = req.query;
+//   const filterObj = {};
+//   for (let filter in filterParams) {
+//     filterObj[`filters.${filter}`] = filterParams[filter];
+//   }
 
-  Place.find(filterObj).then(results => {
-    res.json(results);
-  });
-});
+//   Place.find(filterObj).then(results => {
+//     res.json(results);
+//   });
+// });
 
 app.listen(process.env.PORT || port, () => {
   console.log("Server is listening on port: ", port);
