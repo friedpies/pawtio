2ba0eed238b894ef06c10ccf8c2812ea0867b54e
friedpies <kenmarut@gmail.com>
2019-06-20 19:22:48 -0500

    did deploy

--- server/index.js
+++ server/index.js
@@ -3,12 +3,12 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const port = process.env.PORT || 3002;
-// const { db } = require("../database");
-// const { Place } = require("../database/models");
+const { db } = require("../database");
+const { Place } = require("../database/models");
 
-// const cors = require("cors");
+const cors = require("cors");
 
-// app.use(cors());
+app.use(cors());
 app.use(express.static(path.join(__dirname, "../build")));
 
 // app.get("/api/places", (req, res) => {
