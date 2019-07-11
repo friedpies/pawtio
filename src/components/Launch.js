bc45a68e3678fc43df3e952b826ffb15c7fe0085
Kenneth Marut <kenmarut@gmail.com>
2019-07-11 15:07:04 -0500

    centered heading

--- src/components/Launch.js
+++ src/components/Launch.js
@@ -12,11 +12,12 @@ class Launch extends Component {
     return (
       <Container id="launch-container">
         <div></div>
-        <div>
-          <h1 className="display-1 m-5" id="main-header">
+        <div id="title-container">
+          
+          <h1 className="display-1" id="main-header">
             pawtio
           </h1>
-          <h3 className="mb-5" id="sub-heading">
+          <h3 id="sub-heading">
             search for a dog-friendly place in Austin, TX
           </h3>
           <LocationFilter
