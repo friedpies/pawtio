759b06c075d9f2bcb241dfbd287ff1473e225700
Kenneth Marut <kenmarut@gmail.com>
2019-07-11 16:28:09 -0500

    modified readme

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,10 +1,9 @@
 import React, { Component } from "react";
 import "../styles/Video.scss";
-import video from "../assets/pawtio-rev.mp4";
 
 const Video = props => (
   <video id="background-video" loop autoPlay>
-    <source src={video} type="video/mp4" />
+    <source src="https://kjmporfolio.nyc3.digitaloceanspaces.com/pawtio/pawtio-bg.mp4" type="video/mp4" />
     Your browser does not support the video tag.
   </video>
 );
