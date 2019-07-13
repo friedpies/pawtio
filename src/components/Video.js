873689c643221c53bf9250bfe154b6109cd864e0
Ken <kenmarut@gmail.com>
2019-07-13 08:25:20 -0500

    hotfix: add mute to video

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,9 +1,12 @@
-import React, { Component } from "react";
+import React from "react";
 import "../styles/Video.scss";
 
 const Video = props => (
-  <video id="background-video" loop autoPlay>
-    <source src="https://kjmporfolio.nyc3.digitaloceanspaces.com/pawtio/pawtio-bg.mp4" type="video/mp4" />
+  <video id="background-video" loop autoPlay muted>
+    <source
+      src="https://kjmporfolio.nyc3.digitaloceanspaces.com/pawtio/pawtio-bg.mp4"
+      type="video/mp4"
+    />
     Your browser does not support the video tag.
   </video>
 );
