50a4adac8176d6868ecb61dc97dc8f7c614b9e27
Ken <kenmarut@gmail.com>
2019-07-13 11:15:17 -0500

    update video url

--- src/components/Video.js
+++ src/components/Video.js
@@ -4,7 +4,7 @@ import "../styles/Video.scss";
 const Video = props => (
   <video id="background-video" loop autoPlay muted>
     <source
-      src="https://kjmporfolio.nyc3.digitaloceanspaces.com/pawtio/pawtio-bg.mp4"
+      src="https://kjmporfolio.nyc3.digitaloceanspaces.com/pawtio/pawtio_small.mp4"
       type="video/mp4"
     />
     Your browser does not support the video tag.
