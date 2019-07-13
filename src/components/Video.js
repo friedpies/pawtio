b45d70421b3265c308f0bdff4db48f7c10a2da06
Ken <kenmarut@gmail.com>
2019-07-13 11:31:02 -0500

    feature: modified package json for heroku postbuild and added env variable for video"

--- src/components/Video.js
+++ src/components/Video.js
@@ -3,10 +3,7 @@ import "../styles/Video.scss";
 
 const Video = props => (
   <video id="background-video" loop autoPlay muted>
-    <source
-      src="https://kjmporfolio.nyc3.digitaloceanspaces.com/pawtio/pawtio_small.mp4"
-      type="video/mp4"
-    />
+    <source src={process.env.REACT_APP_VIDEO_URL} type="video/mp4" />
     Your browser does not support the video tag.
   </video>
 );
